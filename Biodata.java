8c99541c6a3ac2bbcef6879bca962eff8de03ae6
Mitsukii <106789003+mitsukii27@users.noreply.github.com>
2022-09-15 07:27:51 +0545

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,12 @@
+class Biodata 
+{
+  public static void main(String args [])
+  {
+  System.out.println("Name:Mitsukii");
+  System.out.println("Address: fairly tale");
+  System.out.println("Date of birth: July 24 2003");
+  System.out.println("Gender: Non binary");
+  System.out.println("Email: mitsukiihasaboyfriend348@gmail.com");
+  System.out.println("Phone: Switched off ");
+  }
+}
